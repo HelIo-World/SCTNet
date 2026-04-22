4cb192766b30a90d583bb5fc3441ab5832c969e0
HelloWorld <1297909082@qq.com>
2026-04-22 17:40:07 +0800

    dataset configuration

---
+++ mmseg/datasets/mars_seg.py
@@ -0,0 +1,23 @@
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+import mmengine.fileio as fileio
+
+@DATASETS.register_module()
+class MarsSegDataset(BaseSegDataset):
+    METAINFO = dict(
+        # 一共 10 个有效类别（索引 0 到 9）
+        classes=('soil', 'sand', 'gravel', 'bedrock', 'rock', 
+                 'track', 'shadow', 'background', 'unknown'),
+        # 为可视化配置的 RGB 颜色表
+        palette=[
+            (128, 0, 0),         
+            (0, 128, 0),   
+            (128, 128, 0),     
+            (0, 0, 128),    
+            (128, 0, 128),    
+            (0, 128, 128),   
+            (128, 128, 128),   
+            (192, 0, 0),     
+            (64, 0, 0)   
+        ]
+    )
